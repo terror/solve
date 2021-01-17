7593b13679b9cdeb651a288cefceb9f190d77121
Liam <terroridk@gmail.com>
2021-01-17 16:52:27 -0500

    Revert back, heroku var undetected

--- client/src/ts/api.ts
+++ client/src/ts/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.PROD ? '/api/' : 'http://localhost:5000/api',
+    baseURL: '/api/',
 });
 
 const judge = axios.create({
